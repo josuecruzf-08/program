0513e6dfccd4cce34b09e6860da513ea0ce7bf81
josuecruzf-08 <josuecruzf98@gmail.com>
2026-08-23 13:54:26 -0600

    Eliminar archivos del repositorio

--- clases/Clase_6.js
+++
@@ -1,3 +0,0 @@
-console.log(Number("25 anos"));
-console.log(parseFloat("25 anos"));
-console.log(parseInt("25 anos"));
--- clases/metodos.js
+++
@@ -1,8 +0,0 @@
-let palabra = "Programacion";
-
-let resultado = palabra.slice(2, 12);
-
-console.log(resultado);
-
-
-
